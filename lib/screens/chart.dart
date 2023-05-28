e7ad112150008dd6729d5f0fed46300d3262da08
AnzathuPindani <anzathu.pindani@nicotechnologies.com>
2023-05-28 16:42:37 +0200

    Push notification

--- lib/screens/chart.dart
+++ lib/screens/chart.dart
@@ -1,10 +1,29 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flatchatapp/widgets/chat_messages.dart';
 import 'package:flatchatapp/widgets/new_messages.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotifications() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    fcm.subscribeToTopic('chat');
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    setupPushNotifications();
+  }
 
   @override
   Widget build(BuildContext context) {
